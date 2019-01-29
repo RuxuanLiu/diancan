b02648ee419473e83c2e5af986a6ff1a99f56923
RuxuanLiu <1282906652@qq.com>
2019-01-29 21:10:50 +0800

    change

--- diancan/order/views.py
+++ diancan/order/views.py
@@ -19,11 +19,12 @@ def weixin_main(request):
         #把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
-        sha1 = hashlib.sha1()
-        map(sha1.update, hashlist)
-        hashcode = sha1.hexdigest()
-        print("handle/GET func: hashcode, signature: ", hashcode, signature)
-        if hashcode == signature:
+        print('[token, timestamp, nonce]', hashlist)
+        hashstr = ''.join([s for s in hashlist]).encode('utf-8')
+        print('hashstr befor sha1', hashstr)
+        hashstr = hashlib.sha1(hashstr).hexdigest()
+        print("handle/GET func: hashcode, signature: ", hashstr, signature)
+        if hashstr == signature:
           return HttpResponse(echostr)
         else:
           return HttpResponse("field")
